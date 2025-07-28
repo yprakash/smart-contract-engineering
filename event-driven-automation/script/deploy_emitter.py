3fac0167ede6fe3f4258a20f625dd141e8465344
Prakash <yprakash.518@gmail.com>
2025-07-29 00:38:15 +0530

    better logging and decode_event_sig

--- event-driven-automation/script/deploy_emitter.py
+++ event-driven-automation/script/deploy_emitter.py
@@ -2,7 +2,7 @@ import time
 
 from utils.w3_utils import deploy_and_verify, load_deployed_contract, send_tx
 
-contract_address = ''
+contract_address = '0x038f007027cBadf831895fe866f4bEa2316E73e9'
 if contract_address:
     print('Assigning emit_contract object to address', contract_address)
     emit_contract = load_deployed_contract(contract_address, 'src/EventEmitter.sol', '0.8.26')
@@ -13,8 +13,9 @@ while True:
     tx_receipt = send_tx(emit_contract.functions.deposit())
     events = emit_contract.events.DepositEvent().process_receipt(tx_receipt)
     print(events)
-    time.sleep(1)
-    tx_receipt = send_tx(emit_contract.functions.withdraw())
-    events = emit_contract.events.WithdrawEvent().process_receipt(tx_receipt)
-    print(events)
-    time.sleep(2)
+    print("========================================")
+    time.sleep(4)
+    # tx_receipt = send_tx(emit_contract.functions.withdraw())
+    # events = emit_contract.events.WithdrawEvent().process_receipt(tx_receipt)
+    # print(events)
+    # time.sleep(2)
--- event-driven-automation/script/upkeep_event_watcher.py
+++ event-driven-automation/script/upkeep_event_watcher.py
@@ -2,7 +2,10 @@ import time
 
 from utils.w3_utils import w3, deploy_and_verify, load_deployed_contract
 
-contract_address = ''
+contract_address = '0xAc4a7733e0A663d1fddA3a1C817119ba60D7DfD7'
+DEPOSIT_SIG = w3.keccak(text="DepositEvent(address)")
+WITHDRAW_SIG = w3.keccak(text="WithdrawEvent(address)")
+
 if contract_address:
     print('Assigning watcher_contract object to address', contract_address)
     watcher_contract = load_deployed_contract(contract_address, 'src/EventWatcher.sol', '0.8.26')
@@ -11,12 +14,20 @@ else:
     print(watcher_contract)
 
 
+def decode_event_sig(event_sig: bytes) -> str:
+    if event_sig == DEPOSIT_SIG:
+        return "DepositEvent"
+    elif event_sig == WITHDRAW_SIG:
+        return "WithdrawEvent"
+    return "UnknownEvent"
+
+
 def handle_event(event):
-    log = watcher_contract.events.UpkeepPerformed().processLog(event)
+    log = watcher_contract.events.UpkeepPerformed().process_log(event)
     logSender = log['args']['by']
-    eventSig = log['args']['eventSig']
+    event_sig = decode_event_sig(log['args']['eventSig'])
     upkeepNumber = log['args']['upkeepNumber']
-    print(f"[NEW UPKEEP] #{upkeepNumber} by {logSender} | EventSig: {eventSig.hex()} | Block: {event['blockNumber']}")
+    print(f"[NEW UPKEEP] #{upkeepNumber} by {logSender} | EventSig: {event_sig} | Block: {event['blockNumber']}")
 
 
 def main():
